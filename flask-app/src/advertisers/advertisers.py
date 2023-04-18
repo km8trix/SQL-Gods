8b6548183300271114ec7543d78ec7b84c17dc89
Spencer Karrat <s.karrat@yahoo.com>
2023-04-18 14:40:43 -0400

    add adid for image GET route

--- flask-app/src/advertisers/advertisers.py
+++ flask-app/src/advertisers/advertisers.py
@@ -71,6 +71,21 @@ def get_ad(adID):
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get ad detail for ad with particular adId
+@advertisers.route('/ads/<adID>/image', methods=['GET'])
+def get_adids_ads(adID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select image from Ad where adId = {0}'.format(adID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
 # Get ad detail for ads with particular adId's metrics
 @advertisers.route('/ads/<adID>/metrics', methods=['GET'])
 def get_ad_metrics(adID):
