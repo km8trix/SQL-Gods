66e7cd3c985c665a9708113d674868441170dd8c
darkjarhead1 <64758101+darkjarhead1@users.noreply.github.com>
2023-04-17 21:59:43 -0400

    name update

--- flask-app/src/advertisers/advertisers.py
+++ flask-app/src/advertisers/advertisers.py
@@ -5,7 +5,7 @@ from src import db
 
 advertisers = Blueprint('advertisers', __name__)
 
-# Get all customers from the DB
+# Get all advertisers from the DB
 @advertisers.route('/advertisers/<advertiserID>', methods=['GET'])
 def get_advertisers(advertiserID):
     cursor = db.get_db().cursor()
@@ -21,7 +21,7 @@ def get_advertisers(advertiserID):
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get all customers from the DB
+# Get all ads from the DB
 @advertisers.route('/ads', methods=['GET'])
 def get_ads():
     query = '''
@@ -41,9 +41,9 @@ def get_ads():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get all customers ids only from the DB
+# Get all advertiser's names and ids only from the DB
 @advertisers.route('/advertisersids', methods=['GET'])
-def get_shows_ids():
+def get_advertisersids():
     cursor = db.get_db().cursor()
     cursor.execute("SELECT advertiserName as label, advertiserId as value FROM Advertiser;")
     row_headers = [x[0] for x in cursor.description]
@@ -56,7 +56,7 @@ def get_shows_ids():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
+# Get ad detail for ad with particular adId
 @advertisers.route('/ads/<adID>', methods=['GET'])
 def get_ad(adID):
     cursor = db.get_db().cursor()
@@ -71,7 +71,7 @@ def get_ad(adID):
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
+# Get ad detail for ads with particular adId's metrics
 @advertisers.route('/ads/<adID>/metrics', methods=['GET'])
 def get_ad_metrics(adID):
     cursor = db.get_db().cursor()
@@ -85,4 +85,20 @@ def get_ad_metrics(adID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Get shows reviews
+@advertisers.route('/shows/<userID>/reviews/edit/<reviewID>', methods=['PUT'])
+def edit_show_review(userID, reviewID):
+    req_data = request.get_json()
+
+    description = req_data['reviewComment']
+    cust_id = req_data['custId']
+    review_id = req_data['reviewId']
+
+    put_stmt = 'UPDATE Review SET reviewComment = ' + description + ' WHERE custId = ' + str(cust_id) + ' AND reviewId = ' + str(review_id) + ';'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(put_stmt)
+    db.get_db().commit()
+    return "Success"
 
